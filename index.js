60a87c61783558e7c70623ff493ceea005907d49
Rubinder Singh <robinsandhu1857@gmail.com>
2023-09-02 15:10:55 +0530

    Added changes for FCC Backend project 3
    Changes for url shortener FCC backend project

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const dns = require('dns');
 const app = express();
 
 // Basic Configuration
@@ -10,13 +11,40 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
+//Add middleware
+app.use(express.urlencoded({ extended: false }));
+
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+let url_mapping = [];
+// Project API endpoint
+
+app.post('/api/shorturl', function(req, res) {
+  const url = req.body['url']
+  const isValidUrl = (/(?<=(http|https):\/\/)[^\/]+/).test(url);
+  if(isValidUrl) {
+    console.log("Valid url = " + isValidUrl);
+    url_mapping.push(url);
+    res.json({ original_url: url,
+      short_url:  url_mapping.length - 1});
+  } else{
+    console.log("Not valid url");
+    res.json({
+      error: 'invalid url'
+    })
+  }
+});
+
+app.get('/api/shorturl/:id?', function(req, res) {
+  if(req.params.id < url_mapping.length) {
+    res.redirect(url_mapping[req.params.id]);
+  } else {
+    res.json({
+      error: 'invalid url'
+    })
+  }
 });
 
 app.listen(port, function() {
